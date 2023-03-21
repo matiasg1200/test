e64c4ddee556681dfebcfa6042c7f8c5f128a63f
Matias Picco <c_mpicco@groupon.com>
2023-03-21 20:52:39 -0300

    Change summary: 	1) Replacing load balancer with ingress on top of a Cluster IP service for both applications 	2) Adding yaml code to deploy echoserver application These changes were not change due to lack of time
    Changes to be committed:
    	deleted:    kubernetes/api application/api-deployment.yaml
    	deleted:    kubernetes/api application/api-lb.yaml
    	new file:   kubernetes/api application/echoserver-deployment.yaml
    	new file:   kubernetes/api application/echoserver-ingress.yaml
    	renamed:    kubernetes/api application/api-namespace.yaml -> kubernetes/api application/echoserver-namespace.yaml
    	new file:   kubernetes/api application/echoserver-service.yaml
    	modified:   kubernetes/statics application/nginx-deployment.yaml
            new file:   kubernetes/statics application/nginx-ingress.yaml
    	renamed:    kubernetes/statics application/nginx-lb.yaml -> kubernetes/statics application/nginx-service.yaml
    	modified:   provision-eks-cluster/main.tf
    	new file:   provision-eks-cluster/terraform.tfstate
    	new file:   provision-eks-cluster/terraform.tfstate.backup

--- provision-eks-cluster/main.tf
+++ provision-eks-cluster/main.tf
@@ -21,10 +21,10 @@ module "vpc" {
   name = "${var.env}-vpc"
 
   cidr = "10.0.0.0/16"
-  azs = ["us-east-1a"]
+  azs = ["us-east-1a", "us-east-1b"]
 
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
