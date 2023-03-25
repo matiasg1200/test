63989b71c081139f30ca0cd2eb86321abde230e9
Matias Picco <c_mpicco@groupon.com>
2023-03-25 18:39:55 -0300

    Changes to be committed: 	1) Adding terraoform code to deploy Kubernetes resoruces 	2) Addoing YAML files to deploy argocd on the cluster

---
+++ kubernetes/kubernetes.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.48.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.1"
+    }
+  }
+}
+
+data "terraform_remote_state" "eks" {
+  backend = "local"
+
+  config = {
+    path = "../provision-eks-cluster/terraform.tfstate"
+  }
+}
+
+# Retrieve EKS cluster information
+provider "aws" {
+  region = data.terraform_remote_state.eks.outputs.region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.aws_eks_cluster.cluster.name
+    ]
+  }
+}
+
+resource "kubernetes_manifest" "argocd_namespace" {
+  manifest = yamldecode(file("./argocd/namespace.yaml"))
+}
+
+resource "kubernetes_manifest" "argocd_install" {
+  manifest = yamldecode(file("./argocd/install.yaml"))
+}
+
