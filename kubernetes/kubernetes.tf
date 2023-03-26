d2b387125c47b3ee546b06cddc3a3ed1ba6aae17
Matias Picco <c_mpicco@groupon.com>
2023-03-25 22:40:57 -0300

    Changes to be committed: 	1) Changing kubernetes_provider for kubectl_provider 		kubernetes_provider does not accept multiple YAML files concatenated into one and argocd installation consists of 56 YAML files. Kubectl_provider solves this issue

--- kubernetes/kubernetes.tf
+++ kubernetes/kubernetes.tf
@@ -29,7 +29,7 @@ data "aws_eks_cluster" "cluster" {
   name = data.terraform_remote_state.eks.outputs.cluster_name
 }
 
-provider "kubernetes" {
+provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
@@ -44,11 +44,25 @@ provider "kubernetes" {
   }
 }
 
-resource "kubernetes_manifest" "argocd_namespace" {
-  manifest = yamldecode(file("./argocd/namespace.yaml"))
+data "kubectl_file_documents" "namespace" {
+    content = file("./argocd/namespace.yaml")
+} 
+
+data "kubectl_file_documents" "argocd" {
+    content = file("./argocd/install.yaml")
 }
 
-resource "kubernetes_manifest" "argocd_install" {
-  manifest = yamldecode(file("./argocd/install.yaml"))
+resource "kubectl_manifest" "namespace" {
+    count     = length(data.kubectl_file_documents.namespace.documents)
+    yaml_body = element(data.kubectl_file_documents.namespace.documents, count.index)
+    
 }
 
+resource "kubectl_manifest" "argocd" {
+    depends_on = [
+      kubectl_manifest.namespace,
+    ]
+    count     = length(data.kubectl_file_documents.argocd.documents)
+    yaml_body = element(data.kubectl_file_documents.argocd.documents, count.index)
+    
+}
